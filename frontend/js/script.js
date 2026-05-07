86f13d7de715e893cd93d029858592bf7cebc1c1
css95 <steilner.carolin@gmail.com>
2026-05-07 15:31:18 +0200

    create book cards and render books functions

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -10,4 +10,58 @@ async function getBooks() {
         return [];
     }
 }
-getBooks();
+
+function createBookCard(book) {
+    const bookCard = document.createElement("div");
+    bookCard.className = "book-card";
+    bookCard.style.cursor = "pointer";
+
+    const coverSection = document.createElement("div");
+    coverSection.className = "book-card__cover-wrapper";
+
+    const bookCover = document.createElement("img");
+    bookCover.className = "book-card__cover";
+    bookCover.src = `http://localhost:1337${book.cover.url}`;
+    bookCover.alt = book.title;
+    bookCover.loading = "lazy";
+    
+    const bookCardBody = document.createElement("div");
+    bookCardBody.className = "book-card__body";
+
+    const bookName = document.createElement("h2");
+    bookName.className = "book-card__name";
+    bookName.textContent = book.title;
+
+    const bookAuthor = document.createElement("h3");
+    bookAuthor.className = "book-card__author";
+    bookAuthor.textContent = book.author;
+
+    const bookRating = document.createElement("p");
+    bookRating.className = "book-card__rating";
+    bookRating.textContent = "★ –/10";
+    
+    coverSection.appendChild(bookCover);
+
+    bookCardBody.appendChild(bookName);
+    bookCardBody.appendChild(bookAuthor);
+    bookCardBody.appendChild(bookRating);
+
+    bookCard.appendChild(coverSection);
+    bookCard.appendChild(bookCardBody);
+
+    return bookCard;
+}
+
+async function renderBooks() {
+
+    const books = await getBooks();
+    const bookList = document.getElementById("bookList");
+
+    books.forEach((book) => {
+        const bookCard = createBookCard(book);
+        bookList.appendChild(bookCard);
+    })
+
+}
+
+renderBooks();
