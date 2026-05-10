e22890f2c49ca0434d0d89d1c6196825e380b8b2
css95 <steilner.carolin@gmail.com>
2026-05-10 14:02:06 +0200

    Add login page with JWT auth

---
+++ frontend/js/login.js
@@ -0,0 +1,29 @@
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    
+    const identifier = document.getElementById("identifier").value;
+    const password = document.getElementById("password").value;
+
+    try {
+
+        const response = await axios.post(`${API_BASE}/auth/local`, {
+            identifier: identifier,
+            password: password
+        });
+
+        const token = response.data.jwt;
+        const user = response.data.user;
+
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user));
+
+        window.location.href = "index.html";
+
+    } catch (error) {
+        const formError = document.getElementById("form-error");
+        formError.textContent = "Wrong email or password.";
+        console.error(error);
+    }
+});
